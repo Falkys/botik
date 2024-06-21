ee2c85db782ae76d02332105eba54c0abbefc713
Falkys <dmvmanea@gmail.com>
2024-06-21 17:38:15 +0300

    config2

---
+++ config.js
@@ -0,0 +1,10 @@
+const v = {
+  "color": "#EA435F",
+  "ownerID": "991606168013242410",
+  "token": `MTAzNjIwMzQ1kBi5iZri4vFzBRpACc9ffkJLzgrjtE`,
+  "api": `${process.env.token}`,
+  "prefix": "*",
+  "clientId": "1036203458291830796",
+}
+
+module.exports = v;
